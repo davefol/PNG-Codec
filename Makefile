89e33ffcdbbbd95122fb7237b820784b041e2b1d
Dave Fol <dof5@cornell.edu>
2022-03-28 19:41:43 -0400

    setting up SDL2

---
+++ Makefile
@@ -0,0 +1,2 @@
+viewer:
+	g++ -std="c++14" viewer.cpp -o viewer -I include -L lib -l SDL2-2.0.0
---
+++ viewer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+int main() {
+    cout << "hello" << endl;
+    return 0;
+}
